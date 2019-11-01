4302e5ae726c795ab5c81839fa543c696980a04d
ratherlongname <shivam.bansal@live.com>
2019-11-01 07:24:43 +0530

    Major restructure
    Restructure program to not use enumerate loops. Generate download link directly from each line. Write all download links to output file in the end. Trim whitespace from line right in the beginning.

--- zippy-download.py
+++ zippy-download.py
@@ -4,50 +4,39 @@ with open("input.txt", "r") as input_file:
 
 # filter valid urls and get their html pages
 import urllib.request
+import re
 
-all_responses = []
-valid_urls = []
+regex_get_mod = r'\((\d+) % (\d+) \+ (\d+) % (\d+)\)'
+regex_get_filename = r'\((\d+) % (\d+) \+ (\d+) % (\d+)\) \+ \"\/(\S+)\"'
+regex_v_in_url = r'/v/'
+regex_filehtml_in_url = r'/file.html[\n]*'
+
+download_links = []
 for line in all_lines:
+    line = line.strip()
+    # check we get response from url in line
     try:
         resp = urllib.request.urlopen(line)
     except ValueError:
         continue
 
-    all_responses.append(resp.read().decode("utf8"))
-    valid_urls.append(line)
+    resp = resp.read().decode("utf8")
 
+    result_v = re.search(regex_v_in_url, line)
+    result_filehtml = re.search(regex_filehtml_in_url, line)
+    result_mod = re.search(regex_get_mod, resp)
+    result_filename = re.search(regex_get_filename, resp)
 
-# create file download links
-import re
-
-def alter_urls(s):
-    ans = re.sub(r'/v/', "/d/", s)
-    ans = re.sub(r'/file.html[\n]*', "/", ans)
-    return ans
-
-## remove file.html and replace v with d in original urls
-download_links = list(map(alter_urls, valid_urls))
-
-## get mod challenge from html and append to url
-regex_get_mod = r'\((\d+) % (\d+) \+ (\d+) % (\d+)\)'
-all_mods = []
-for resp in all_responses:
-    result = re.search(regex_get_mod, resp)
-    mod = (int(result.group(1)) % int(result.group(2))) + (int(result.group(3)) % int(result.group(4)))
-    all_mods.append(mod)
-
-for i, mod in enumerate(all_mods):
-    download_links[i] = download_links[i] + str(mod) + '/'
-
-# get filename from html and append to url
-regex_get_filename = r'\((\d+) % (\d+) \+ (\d+) % (\d+)\) \+ \"\/(\S+)\"'
-all_names = []
-for resp in all_responses:
-    result = re.search(regex_get_filename, resp)
-    all_names.append(result.group(5))
-
-for i, name in enumerate(all_names):
-    download_links[i] = download_links[i] + name
+    if result_v is None or result_filehtml is None or result_mod is None or result_filename is None:
+        continue
+    else:
+        url = line
+        url = re.sub(regex_v_in_url, "/d/", url)
+        url = re.sub(regex_filehtml_in_url, "/", url)
+        mod = (int(result_mod.group(1)) % int(result_mod.group(2))) + (int(result_mod.group(3)) % int(result_mod.group(4)))
+        url = url + str(mod) + "/"
+        url = url + result_filename.group(5)
+        download_links.append(url)
 
 # write all download links to output.txt
 with open("output.txt", "w") as output_file:
